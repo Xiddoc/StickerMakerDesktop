236af37e4a3d343fb1de9bb96ce600931002a727
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-26 22:26:53 +0200

    Use loader instead of Python code (for berevity)

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 Main script / entry script.
 """
 
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtWidgets import QMainWindow, QApplication, QSizePolicy, QWidget, QVBoxLayout, QLayout, QLabel, QPushButton, \
-	QHBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from qtpy import uic
 
 
 class StickerMakerApp(QMainWindow):
@@ -16,72 +15,9 @@ class StickerMakerApp(QMainWindow):
 		# Initialize main window
 		super().__init__()
 
-		self.Main = QWidget(self)
-		size_policy = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
-		
-		size_policy.setHeightForWidth(self.Main.sizePolicy().hasHeightForWidth())
-		self.Main.setSizePolicy(size_policy)
+		# Load the UI from a designer file
+		uic.loadUi('stickermaker.ui', self)
 
-		self.verticalLayoutWidget = QWidget(self.Main)
-		self.verticalLayoutWidget.setGeometry(QtCore.QRect(-1, -1, 641, 481))
-		self.Vertical = QVBoxLayout(self.verticalLayoutWidget)
-		self.Vertical.setSizeConstraint(QLayout.SetMaximumSize)
-		self.Vertical.setContentsMargins(0, 0, 0, 0)
-
-		self.ImageDrop = QLabel(self.verticalLayoutWidget)
-		size_policy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-		size_policy.setHeightForWidth(self.ImageDrop.sizePolicy().hasHeightForWidth())
-		self.ImageDrop.setSizePolicy(size_policy)
-		self.ImageDrop.setMinimumSize(QtCore.QSize(0, 400))
-		font = QtGui.QFont()
-		font.setFamily("Segoe UI")
-		font.setPointSize(28)
-		self.ImageDrop.setFont(font)
-		self.ImageDrop.setAcceptDrops(True)
-		self.ImageDrop.setText("<html><head/><body>"
-		                       "<p>Drag-and-drop a file here</p>"
-		                       "<p>or paste from your Clipboard</p>"
-		                       "</body></html>")
-		self.ImageDrop.setAlignment(QtCore.Qt.AlignCenter)
-
-		self.Vertical.addWidget(self.ImageDrop)
-		self.Buttons = QHBoxLayout()
-		self.Buttons.setSizeConstraint(QLayout.SetMaximumSize)
-		self.Buttons.setSpacing(0)
-
-		self.SaveToFile = QPushButton(self.verticalLayoutWidget)
-		size_policy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-		size_policy.setHeightForWidth(self.SaveToFile.sizePolicy().hasHeightForWidth())
-		self.SaveToFile.setSizePolicy(size_policy)
-		font = QtGui.QFont()
-		font.setFamily("Segoe UI")
-		font.setPointSize(24)
-		self.SaveToFile.setFont(font)
-		self.SaveToFile.setText("Save to file")
-
-		self.Buttons.addWidget(self.SaveToFile)
-		self.CopyToClipboard = QPushButton(self.verticalLayoutWidget)
-		size_policy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-		size_policy.setHeightForWidth(self.CopyToClipboard.sizePolicy().hasHeightForWidth())
-		self.CopyToClipboard.setSizePolicy(size_policy)
-		font = QtGui.QFont()
-		font.setFamily("Segoe UI")
-		font.setPointSize(24)
-		self.CopyToClipboard.setFont(font)
-		self.CopyToClipboard.setText("Copy to Clipboard")
-
-		self.Buttons.addWidget(self.CopyToClipboard)
-		self.Vertical.addLayout(self.Buttons)
-		self.setCentralWidget(self.Main)
-
-		# Create a size policy for the window
-		size_policy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-		size_policy.setHeightForWidth(self.sizePolicy().hasHeightForWidth())
-		self.setSizePolicy(size_policy)
-		# Resize the window (fixed)
-		self.setFixedSize(640, 480)
-		# Name the window
-		self.setWindowTitle("Sticker Maker [Desktop]")
 		# Show the window
 		self.show()
 
