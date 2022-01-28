f157fd41d154316a07a6b90696e46d24cdf774d9
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-01-28 15:47:18 +0200

    Initialize pack code

--- packer/StickerPack.py
+++ packer/StickerPack.py
@@ -1,6 +1,9 @@
 """
 Sticker Pack class.
 """
+from os import mkdir
+from shutil import rmtree
+
 from typing import List
 
 from PIL.Image import Image
@@ -14,14 +17,24 @@ class StickerPack:
 	images: List[Image]
 
 	def __init__(self) -> None:
-		# Init the list
-		self.images = []
+		# Initiate the pack for the first time
+		self.init_pack()
 
 	def init_pack(self) -> None:
 		"""
 		Creates a new 'temp' directory if it doesn't exist,
 		or cleans out the directory if it is full of old files.
 		"""
+		# Clear out the image list
+		self.images = []
+		try:
+			# Remove the temp directory
+			rmtree("temp", ignore_errors=True)
+			# Make the temp directory
+			mkdir("temp")
+		except FileExistsError:
+			# If it already exists, then ignore
+			pass
 
 	def compress_pack(self, file_path: str) -> None:
 		"""
